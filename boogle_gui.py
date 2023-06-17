32b5e6aa4b8b818b505f61db1f783ece8aa2dbae
agam-hershko <136508903+agam-hershko@users.noreply.github.com>
2023-06-17 21:17:18 +0300

    Revert "Finish the game"

--- boogle_gui.py
+++ boogle_gui.py
@@ -243,21 +243,14 @@ class BoogleGUI:
         self.__words_list.delete(0, tk.END)
 
     def __create_start_button(self):
-        """ The function activates start button """
-        # Creating start button
-        self.__start_button = tk.Button(self.__outer_frame, text="START",
-                                        **BUTTON_STYLE)
-        self.__start_button.place(relheight=0.05, relwidth=0.1, relx=0.1,
-                                  rely=0.2)
+        """ The function creates start button """
+        button = tk.Button(self.__outer_frame, text="START", **BUTTON_STYLE)
+        button.place(relheight=0.05, relwidth=0.1, relx=0.1, rely=0.2)
 
         def click_on_start(event):
-            # Ignore the click event if the button is disabled when it
-            if self.__start_button.cget("state") == 'disabled':
-                return None
-
             # If the game is played, change label from start to reset
-            if self.__start_button.cget("text") == "START":
-                self.__start_button.config(text="RESET")
+            if button.cget("text") == "START":
+                button.config(text="RESET")
 
             self.__create_timer()
             self.__clear_word()
@@ -266,20 +259,16 @@ class BoogleGUI:
             self.__reset_words_list()
 
         # Handling events
-        self.__start_button.bind("<Button-1>", click_on_start)  # Click start
-        self.__start_button.bind("<Enter>",  # Get over button
-                                 lambda event: self.__start_button.config(
-                                     bg=BUTTON_HOVER_COLOR))
-        self.__start_button.bind("<Leave>",  # Leave widget area
-                                 lambda event: self.__start_button.config(
-                                     bg=REGULAR_COLOR))
+        button.bind("<Button-1>", click_on_start)  # Click on start button
+        button.bind("<Enter>",  # Get over button
+                    lambda event: button.config(bg=BUTTON_HOVER_COLOR))
+        button.bind("<Leave>",  # Leave widget area
+                    lambda event: button.config(bg=REGULAR_COLOR))
 
     def __create_submit_button(self):
         """ The function creates submit button """
-        self.__submit_button = tk.Button(self.__outer_frame, text="SUBMIT",
-                                         **BUTTON_STYLE)
-        self.__submit_button.place(relheight=0.05, relwidth=0.1, relx=0.35,
-                                   rely=0.2)
+        button = tk.Button(self.__outer_frame, text="SUBMIT", **BUTTON_STYLE)
+        button.place(relheight=0.05, relwidth=0.1, relx=0.35, rely=0.2)
 
         def click_on_submit(event):
             current_word = self.__get_word_from_letters()
@@ -302,34 +291,27 @@ class BoogleGUI:
             self.__clear_word()
 
         # Handling events
-        self.__submit_button.bind("<Button-1>",
-                                  click_on_submit)  # Click on submit
-        self.__submit_button.bind("<Enter>",  # Get over button
-                                  lambda event: self.__submit_button.config(
-                                      bg=BUTTON_HOVER_COLOR))
-        self.__submit_button.bind("<Leave>",  # Leave widget area
-                                  lambda event: self.__submit_button.config(
-                                      bg=REGULAR_COLOR))
+        button.bind("<Button-1>", click_on_submit)  # Click on submit
+        button.bind("<Enter>",  # Get over button
+                    lambda event: button.config(bg=BUTTON_HOVER_COLOR))
+        button.bind("<Leave>",  # Leave widget area
+                    lambda event: button.config(bg=REGULAR_COLOR))
 
     def __create_clear_button(self):
         """ The function creates clear button """
-        self.__clear_button = tk.Button(self.__outer_frame, text="CLEAR",
-                                        **BUTTON_STYLE)
-        self.__clear_button.place(relheight=0.05, relwidth=0.1, relx=0.6,
-                                  rely=0.2)
+        button = tk.Button(self.__outer_frame, text="CLEAR",
+                           **BUTTON_STYLE)
+        button.place(relheight=0.05, relwidth=0.1, relx=0.6, rely=0.2)
 
         def click_on_clear(event):
             self.__clear_word()
 
         # Handling events
-        self.__clear_button.bind("<Button-1>",
-                                 click_on_clear)  # Click on submit
-        self.__clear_button.bind("<Enter>",  # Get over button
-                                 lambda event: self.__clear_button.config(
-                                     bg=BUTTON_HOVER_COLOR))
-        self.__clear_button.bind("<Leave>",  # Leave widget area
-                                 lambda event: self.__clear_button.config(
-                                     bg=REGULAR_COLOR))
+        button.bind("<Button-1>", click_on_clear)  # Click on submit
+        button.bind("<Enter>",  # Get over button
+                    lambda event: button.config(bg=BUTTON_HOVER_COLOR))
+        button.bind("<Leave>",  # Leave widget area
+                    lambda event: button.config(bg=REGULAR_COLOR))
 
     def __create_close_button(self):
         """ The function creates close button which is close the window """
@@ -381,98 +363,10 @@ class BoogleGUI:
             if elapsed_time < TIME_IN_SECS:  # Stop the timer after 3 minutes
                 self.__timer.after(1000, update_timer)
             else:
-                self.__finish_the_game()
+                print("sad")
 
         self.__start_time = time.time()
         update_timer()
-
-    def __create_yes_button(self):
-        button = tk.Button(self.__board, text="YES",
-                           **BUTTON_STYLE)
-        button.place(relheight=0.15, relwidth=0.1, relx=0.65, rely=0.7)
-
-        def click_on_yes(event):
-            # Recreate board
-            self.__configure_board()
-            self.__create_letters_buttons()
-            # Activate buttons
-            self.__start_button.config(state="normal")
-            self.__submit_button.config(state="normal")
-            self.__clear_button.config(state="normal")
-            # Reset game objects
-            self.__create_timer()
-            self.__score.config(text="score: 0")
-            self.__reset_words_list()
-
-        # Handling events
-        button.bind("<Button-1>", click_on_yes)  # Click on submit
-        button.bind("<Enter>",  # Get over button
-                    lambda event: button.config(bg=BUTTON_HOVER_COLOR))
-        button.bind("<Leave>",  # Leave widget area
-                    lambda event: button.config(bg=REGULAR_COLOR))
-
-    def __create_no_button(self):
-        button = tk.Button(self.__board, text="NO",
-                           **BUTTON_STYLE)
-        button.place(relheight=0.15, relwidth=0.1, relx=0.8, rely=0.7)
-
-        def click_on_no(event):
-            # Todo- add pop window with message and total score (also in close)
-            self.__window.destroy()
-
-        # Handling events
-        button.bind("<Button-1>", click_on_no)  # Click on submit
-        button.bind("<Enter>",  # Get over button
-                    lambda event: button.config(bg=BUTTON_HOVER_COLOR))
-        button.bind("<Leave>",  # Leave widget area
-                    lambda event: button.config(bg=REGULAR_COLOR))
-
-    def __finish_the_game(self):
-        self.__board = tk.Frame(self.__outer_frame)
-        self.__board.place(relheight=0.6, relwidth=0.6, relx=0.1, rely=0.3)
-
-        # Disable buttons and clear word on display
-        self.__start_button.config(state="disabled")
-        self.__submit_button.config(state="disabled")
-        self.__clear_button.config(state="disabled")
-        self.__clear_word()
-
-        # Create label with information of ending game
-        game_message = tk.Label(self.__board, **LABEL_STYLE)
-        game_message.place(relheight=0.15, relwidth=0.3, relx=0.35,
-                           rely=0.1)
-        game_message.config(text="Your time is up!")
-
-        # Creating label with information of score in last game
-        last_score = tk.Label(self.__board, **LABEL_STYLE)
-        last_score.config(text=self.__score.cget("text"))
-        last_score.place(relheight=0.15, relwidth=0.2, relx=0.4,
-                         rely=0.4)
-
-        # Creating label with question about new game
-        new_game = tk.Label(self.__board, **LABEL_STYLE)
-        new_game.place(relheight=0.15, relwidth=0.5, relx=0.1,
-                       rely=0.7)
-        new_game.config(text="Do you want to play again?")
-        # Create buttons to answer the question
-        self.__create_yes_button()
-        self.__create_no_button()
-
-    def __create_close_button(self):
-        """ The function creates close button which is close the window """
-        button = tk.Button(self.__outer_frame, text="CLOSE",
-                           **BUTTON_STYLE)
-        button.place(relheight=0.05, relwidth=0.1, relx=0.9, rely=0.0)
-
-        def click_on_close(event):
-            self.__window.destroy()
-
-        # Handling events
-        button.bind("<Button-1>", click_on_close)  # Click on submit
-        button.bind("<Enter>",  # Get over button
-                    lambda event: button.config(bg=BUTTON_HOVER_COLOR))
-        button.bind("<Leave>",  # Leave widget area
-                    lambda event: button.config(bg=REGULAR_COLOR))
 
     def run(self):
         """ The function runs the game """
