7c0f2a1b9c8d43b191d37d01a773b27daa1eac17
itzhakfagebaume <135975436+itzhakfagebaume@users.noreply.github.com>
2023-06-18 13:00:51 +0300

    Add files via upload

--- boogle_gui.py
+++ boogle_gui.py
@@ -19,6 +19,7 @@ BUTTON_STYLE = {"font": ("Courier", 25),
 LABEL_STYLE = {"font": ("Courier", 15), "bg": REGULAR_COLOR}
 
 
+
 class BoogleGUI:
     """ Class that implements gui of boogle game and has attributes of objects
     on the screen """
@@ -68,10 +69,6 @@ class BoogleGUI:
         self.__words_list = tk.Listbox(self.__outer_frame, **LABEL_STYLE)
         self.__words_list.place(relheight=0.6, relwidth=0.2, relx=0.75,
                                 rely=0.3)
-        scrollbar = tk.Scrollbar(self.__outer_frame, orient="vertical")
-        scrollbar.config(command=self.__words_list.yview)
-        scrollbar.place(relheight=0.6, relwidth=0.02, relx=0.95, rely=0.3)
-        self.__words_list.config(yscrollcommand=scrollbar.set)
 
         # Creates game objects
         self.__letters_in_word = []
@@ -80,6 +77,26 @@ class BoogleGUI:
         self.__create_submit_button()
         self.__create_clear_button()
         self.__create_close_button()
+        self.__window.bind("<Configure>", self.__configure_window)
+
+    
+    
+    def __configure_window(self,event):
+            """Method to configure the window size and adjust button font size accordingly"""
+            window_width = self.__window.winfo_width()
+            window_height = self.__window.winfo_height()
+            button_font_size = min(window_width, window_height) // 25
+
+            # Update font size for letter buttons
+            for row in range(BOARD_ROWS):
+                for col in range(BOARD_COLS):
+                    button = self.__letters_in_board[row][col]
+                    button.config(font=("Courier", button_font_size))
+
+            # Update font size for other buttons and labels
+            for widget in [self.__word_display, self.__timer, self.__score, self.__total_score, self.__words_list, self.__start_button, self.__submit_button, self.__clear_button]:
+                widget.config(font=("Courier", button_font_size))
+
 
     def __configure_board(self):
         """ The function Configures board frame (for letters buttons) """
@@ -289,9 +306,9 @@ class BoogleGUI:
             current_word = self.__get_word_from_letters()
             words = create_set("boggle_dict.txt")
 
-            if (current_word in words
-                    and current_word not in
-                    self.__words_list.get(0, "end")):  # If word not in list
+            if current_word in words \
+                    and current_word not in \
+                    self.__words_list.get(0, "end"):  # If word not in list
                 current_score = len(current_word) ** 2
                 score = self.__score.cget("text").split(":")[1]
                 self.__score.config(
@@ -334,6 +351,22 @@ class BoogleGUI:
         self.__clear_button.bind("<Leave>",  # Leave widget area
                                  lambda event: self.__clear_button.config(
                                      bg=REGULAR_COLOR))
+
+    def __create_close_button(self):
+        """ The function creates close button which is close the window """
+        self.__close_button = tk.Button(self.__outer_frame, text="CLOSE",
+                           **BUTTON_STYLE)
+        self.__close_button.place(relheight=0.05, relwidth=0.1, relx=0.9, rely=0.0)
+
+        def click_on_close(event):
+            self.__window.destroy()
+
+        # Handling events
+        self.__close_button.bind("<Button-1>", click_on_close)  # Click on submit
+        self.__close_button.bind("<Enter>",  # Get over button
+                    lambda event: self.__close_button.config(bg=BUTTON_HOVER_COLOR))
+        self.__close_button.bind("<Leave>",  # Leave widget area
+                    lambda event: self.__close_button.config(bg=REGULAR_COLOR))
 
     def __present_time(self, minutes, seconds):
         """
@@ -416,11 +449,6 @@ class BoogleGUI:
                     lambda event: button.config(bg=REGULAR_COLOR))
 
     def __finish_the_game(self):
-        """
-        function finishes the game: create frame for message and disables
-        buttons and create game object on frame (game message, score, message
-        about new game and answer buttons)
-        """
         self.__board = tk.Frame(self.__outer_frame)
         self.__board.place(relheight=0.6, relwidth=0.6, relx=0.1, rely=0.3)
 
